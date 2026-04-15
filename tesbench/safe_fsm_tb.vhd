4b7a766d47a2800550ceb0be454ab5a6c07ebd84
Adameno1 <146659419+Adameno1@users.noreply.github.com>
2026-04-15 19:25:42 +0200

    Update safe_fsm_tb.vhd

--- tesbench/safe_fsm_tb.vhd
+++ tesbench/safe_fsm_tb.vhd
@@ -12,68 +12,94 @@ end tb_safe_fsm;
 architecture tb of tb_safe_fsm is
 
     component safe_fsm
-        port (clk         : in std_logic;
-              rst         : in std_logic;
-              btn_store   : in std_logic;
-              btn_compare : in std_logic;
-              store_en    : out std_logic;
-              compare_en  : out std_logic;
-              digit_index : out std_logic_vector (1 downto 0));
+        port (
+            clk         : in  std_logic;
+            rst         : in  std_logic;
+            btn_store   : in  std_logic;
+            btn_compare : in  std_logic;
+            store_en    : out std_logic;
+            compare_en  : out std_logic;
+            digit_index : out std_logic_vector(1 downto 0)
+        );
     end component;
 
-    signal clk         : std_logic;
-    signal rst         : std_logic;
-    signal btn_store   : std_logic;
-    signal btn_compare : std_logic;
+    signal clk         : std_logic := '0';
+    signal rst         : std_logic := '0';
+    signal btn_store   : std_logic := '0';
+    signal btn_compare : std_logic := '0';
     signal store_en    : std_logic;
     signal compare_en  : std_logic;
-    signal digit_index : std_logic_vector (1 downto 0);
+    signal digit_index : std_logic_vector(1 downto 0);
 
-    constant TbPeriod : time := 1000 ns; -- ***EDIT*** Put right period here
-    signal TbClock : std_logic := '0';
-    signal TbSimEnded : std_logic := '0';
+    constant TbPeriod  : time := 10 ns;
+    signal TbClock     : std_logic := '0';
+    signal TbSimEnded  : std_logic := '0';
 
 begin
 
     dut : safe_fsm
-    port map (clk         => clk,
-              rst         => rst,
-              btn_store   => btn_store,
-              btn_compare => btn_compare,
-              store_en    => store_en,
-              compare_en  => compare_en,
-              digit_index => digit_index);
+        port map (
+            clk         => clk,
+            rst         => rst,
+            btn_store   => btn_store,
+            btn_compare => btn_compare,
+            store_en    => store_en,
+            compare_en  => compare_en,
+            digit_index => digit_index
+        );
 
-    -- Clock generation
+    
     TbClock <= not TbClock after TbPeriod/2 when TbSimEnded /= '1' else '0';
-
-    -- ***EDIT*** Check that clk is really your main clock signal
     clk <= TbClock;
 
     stimuli : process
     begin
-        -- ***EDIT*** Adapt initialization as needed
-        btn_store <= '0';
+       
+        rst         <= '0';
+        btn_store   <= '0';
         btn_compare <= '0';
 
-        -- Reset generation
-        -- ***EDIT*** Check that rst is really your reset signal
+        
         rst <= '1';
-        wait for 100 ns;
+        wait for 3 * TbPeriod;
         rst <= '0';
-        wait for 100 ns;
+        wait for 2 * TbPeriod;
 
-        -- ***EDIT*** Add stimuli here
-        wait for 100 * TbPeriod;
+        
+        btn_store <= '1';
+        wait for TbPeriod;
+        btn_store <= '0';
+        wait for 2 * TbPeriod;
 
-        -- Stop the clock and hence terminate the simulation
+     
+        btn_store <= '1';
+        wait for TbPeriod;
+        btn_store <= '0';
+        wait for 2 * TbPeriod;
+
+  
+        btn_store <= '1';
+        wait for TbPeriod;
+        btn_store <= '0';
+        wait for 2 * TbPeriod;
+
+  
+        btn_store <= '1';
+        wait for TbPeriod;
+        btn_store <= '0';
+        wait for 2 * TbPeriod;
+
+        btn_compare <= '1';
+        wait for TbPeriod;
+        btn_compare <= '0';
+        wait for 4 * TbPeriod;
+
+       
         TbSimEnded <= '1';
         wait;
     end process;
 
 end tb;
-
--- Configuration block below is required by some simulators. Usually no need to edit.
 
 configuration cfg_tb_safe_fsm of tb_safe_fsm is
     for tb
